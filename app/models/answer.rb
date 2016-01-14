e15f7ecf20ac609bf840f050c9b1a71a4bcee767
Sue Li <zhl059@mail.harvard.edu>
2016-01-14 20:15:18 +0100

    added user fields to quiz

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
-  belongs_to :quiz
+  # belongs_to :quiz
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :questions
   end
 
-  resources :answers
+  # resources :answers
 
   resources :users do
     resources :quizzes
